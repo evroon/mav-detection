e7d95a91f1ddf9f97e8ebd6e5b8202e447803ea5
Erik Vroon <erik.vroon22@gmail.com>
2021-01-12 18:59:06 +0100

    Implement detection algorithm

--- main.py
+++ main.py
@@ -8,30 +8,132 @@ from   farneback import Farneback
 # sequence = '06'
 # capture, N = utils.get_kitti_capture(sequence)
 
-sequence = 'uav0000244_01440_v'
-capture, N = utils.get_vis_drone_capture(sequence)
+# sequence = 'uav0000244_01440_v'
+# capture, N = utils.get_vis_drone_capture(sequence)
 
-# capture, N = utils.get_train_capture()
+# sequence = 'indoor-modern/warehouse-transition'
 
-output = utils.get_output('detection', capture)
-feature_detector = LucasKanade(capture, output)
+sequence = 'dataset2'
+orig_capture, N = utils.get_cenek_capture(sequence, 'cam1')
+flow_capture = cv2.VideoCapture('media/flownet2v2.mp4')
+capture_size = utils.get_capture_size(flow_capture)
 
+output = utils.get_output('detection', capture_size=(capture_size[0] * 2, capture_size[1]))
+# feature_detector = LucasKanade(capture, output)
+color = (0, 255, 0)
 i = 0
+enable_preview = True
+prev_estimate = None
+
+# print(utils.get_frame_count(orig_capture))
+# print(utils.get_frame_count(flow_capture))
+# print(utils.get_fps(orig_capture))
+# print(utils.get_fps(flow_capture))
 
 try:
+    ann_path = utils.get_cenek_annotation(sequence, 'cam1')
+    f = open(ann_path, 'r')
+
     while i < N - 1:
         if i % int(N / 10) == 0:
             print('{:.2f}'.format(i / N * 100) + '%', i, '/', N)
 
-        frame = feature_detector.process()
-        cv2.imshow('frame', frame)
-        output.write(frame)
+        # frame = feature_detector.process()
+        s1, orig_frame = orig_capture.read()
+        s2, flow_frame = flow_capture.read()
+
+        if not s1 or not s2:
+            break
+
+        if i < 1500:
+            i += 1
+            line = f.readline()
+            continue
+
+        orig_frame = orig_frame[::-1, ::-1, :]
+
+        # ann_path = utils.get_midgard_annotation(sequence, i)
+
+        line = f.readline()
+        values = line.replace('  ', ' ').replace('  ', ' ').split(' ')
+        size = 60
+        ground_truth = utils.Rectangle.from_center((int(float(values[2])), int(float(values[1]))), (size, size))
+        ground_truth.to_int()
+
+        # with open(ann_path, 'r') as f:
+        #     for line in f.readlines():
+        #         values = line.split(',')
+        #         values = [round(float(x)) for x in values]
+        #         topleft = (values[1], values[2])
+        #         bottomright = (values[1] + values[3], values[2] + values[4])
+        #         orig_frame = cv2.rectangle(orig_frame, topleft, bottomright, color, 2)
+        #         flow_frame = cv2.rectangle(flow_frame, topleft, bottomright, color, 2)
+
+        flow_hsv = cv2.cvtColor(flow_frame, cv2.COLOR_BGR2HSV)
+        # flow_hsv[..., 0] = flow_hsv[..., 1]
+        # flow_hsv[..., 1] = 255
+        # flow_hsv[..., 2] = 255
+        # flow_frame = cv2.cvtColor(flow_hsv, cv2.COLOR_HSV2BGR)
+
+        v = flow_hsv[..., 1]
+        v_max = v.max()
+        max_x, max_y = [], []
+        window_size = 60
+        center = np.unravel_index(v.argmax(), v.shape)
+        threshold = 50
+
+        for x in range(max(0, center[0] - window_size), min(v.shape[0], center[0] + window_size)):
+            for y in range(max(0, center[1] - window_size), min(v.shape[1], center[1] + window_size)):
+                if v[x, y] > v_max - threshold:
+                    max_x.append(x)
+                    max_y.append(y)
+
+        max_x = np.array(max_x)
+        max_y = np.array(max_y)
+        estimate = utils.Rectangle.from_center((int(np.average(max_x)) + size // 2, int(np.average(max_y))), (size, size))
+        estimate_int = utils.Rectangle(estimate.topleft, estimate.size)
+        estimate_int.to_int()
+
+        background = flow_hsv.copy()
+        background[estimate_int.get_bottom():estimate_int.get_top(), estimate_int.get_left():estimate_int.get_right()] = np.nan
+        SNR = v_max ** 2.0 / np.var(background)
+
+        aoi = utils.Rectangle.calculate_aoi(ground_truth, estimate) / estimate.get_area()
+
+        if prev_estimate is not None:
+            diff = np.array(estimate.get_center()) - np.array(prev_estimate.get_center())
+            diff_mag = np.sqrt(diff[0] ** 2.0 + diff[1])
+
+            if diff_mag < 100.0:
+                orig_frame = cv2.rectangle(np.array(orig_frame), estimate_int.get_topleft(), estimate_int.get_bottomright(), (0, 0, 0), 5)
+
+                cv2.putText(orig_frame,
+                    f'SNR = {SNR:.02f}, IoU={aoi:.02f}',
+                    (estimate_int.get_top(), estimate_int.get_left() - 5),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (0, 0, 0),
+                    2
+                )
+
+        if ground_truth.get_center() != (0.0, 0.0):
+            orig_frame = cv2.rectangle(np.array(orig_frame), ground_truth.get_topleft(), ground_truth.get_bottomright(), (0, 255, 0), 3)
+
+        prev_estimate = estimate
+        orig_frame = cv2.resize(orig_frame, capture_size, interpolation = cv2.INTER_AREA)
+        output.write(np.hstack((orig_frame, flow_frame)))
         i += 1
 
-        k = cv2.waitKey(30) & 0xff
-        if k == 27:
-            break
+        if enable_preview:
+            cv2.imshow('original', orig_frame)
+            cv2.imshow('flow', flow_frame)
+
+            k = cv2.waitKey(1) & 0xff
+            if k == 27:
+                break
 finally:
-    capture.release()
+    orig_capture.release()
+    flow_capture.release()
     output.release()
     cv2.destroyAllWindows()
+    f.close()
--- utils.py
+++ utils.py
@@ -3,14 +3,77 @@ import os
 import numpy as np
 
 
-def get_output(filename, capture, is_grey=False):
+class Rectangle:
+    def __init__(self, topleft, size):
+        self.topleft = topleft
+        self.size = size
+
+    @classmethod
+    def from_center(cls, center, size):
+        return Rectangle(
+            (center[0] - size[0] / 2, center[1] - size[1] / 2),
+            size
+        )
+
+    def to_int(self):
+        self.topleft = (int(self.topleft[0]), int(self.topleft[1]))
+        self.size = (int(self.size[0]), int(self.size[1]))
+
+    def get_topleft(self):
+        return (self.topleft[1], self.topleft[0])
+
+    def get_bottomright(self):
+        return (self.topleft[1] + self.size[1], self.topleft[0] + self.size[0])
+
+    def get_center(self):
+        return (self.topleft[1] + self.size[1] / 2, self.topleft[0] + self.size[0] / 2)
+
+    def get_left(self):
+        return self.topleft[0]
+
+    def get_right(self):
+        return self.topleft[0] + self.size[0]
+
+    def get_top(self):
+        return self.topleft[1]
+
+    def get_bottom(self):
+        return self.topleft[1] + self.size[1]
+
+    def get_area(self):
+        return self.size[0] * self.size[1]
+
+    @classmethod
+    def calculate_aoi(cls, r1, r2):
+        left = max(r1.get_left(), r2.get_left())
+        right = min(r1.get_right(), r2.get_right())
+        bottom = min(r1.get_bottom(), r2.get_bottom())
+        top = max(r1.get_top(), r2.get_top())
+
+        return (right - left) * (bottom - top)
+
+
+
+def get_capture_size(capture):
+    return (int(capture.get(3)), int(capture.get(4)))
+
+def get_output(filename, capture=None, capture_size=None, is_grey=False):
     path = 'media/output/{}.mp4'.format(filename)
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    capture_size = (int(capture.get(3)), int(capture.get(4)))
+
+    if capture_size is None:
+        capture_size = get_capture_size(capture)
+
     return cv2.VideoWriter(path, fourcc, 30.0, capture_size, not is_grey)
 
 def get_sequence_length(path):
     return count_dir(path)
+
+def get_frame_count(cap):
+    return int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+def get_fps(cap):
+    return int(cap.get(cv2.CAP_PROP_FPS))
 
 # Vis-Drone
 def get_vis_drone_path(sequence):
@@ -32,13 +95,41 @@ def get_kitti_capture(sequence):
     path = get_kitti_path(sequence)
     return cv2.VideoCapture(path + '/%6d.png'), count_dir(path)
 
+# MIDGARD
+def get_midgard_path(sequence):
+    kitti_path = os.environ['MIDGARD_PATH']
+    img_path = f'{kitti_path}/{sequence}/images'
+    ann_path = f'{kitti_path}/{sequence}/annotation'
+    return img_path, ann_path
+
+def get_midgard_capture(sequence):
+    path = get_midgard_path(sequence)[0]
+    return cv2.VideoCapture(f'{path}/image_%5d.png'), count_dir(path)
+
+def get_midgard_annotation(sequence, frame):
+    path = get_midgard_path(sequence)[1]
+    return f'{path}/annot_{frame:05d}.csv'
+
+# Cenek Albl et al.
+def get_cenek_path(sequence, camera):
+    cenek_path = os.environ['CENEK_PATH']
+    img_path = f'{cenek_path}/{sequence}/{camera}.mp4'
+    ann_path = f'{cenek_path}/{sequence}/detections/{camera}.txt'
+    return img_path, ann_path
+
+def get_cenek_capture(sequence, camera):
+    cap = cv2.VideoCapture(get_cenek_path(sequence, camera)[0])
+    return cap, get_frame_count(cap)
+
+def get_cenek_annotation(sequence, camera):
+    return get_cenek_path(sequence, camera)[1]
+
 def get_train_capture():
     path = 'media/train.mp4'
     return cv2.VideoCapture(path), 1e4
 
 def count_dir(path):
     return len(os.listdir(path))
-
 
 # Math utils
 def line_intersection(line1, line2):
