dbde8eb98bb8ba26d0dd8d5c53e050874017b462
Erik Vroon <erik.vroon22@gmail.com>
2021-02-26 16:43:44 +0100

    Move scripts to bash folder
    Former-commit-id: b67a7416c4a7ee8bccbf4858ec65ea06b86d3c4d [formerly 342953d531a2773278c38080348d967b446a7435]
    Former-commit-id: 91f87f2935ebbdf9212d949baa0eec22c47eb7f4

---
+++ etc/bash/scp_annotations.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+scp -r erik@192.168.178.235:/home/erik/tno/datasets/MIDGARD/countryside-natural/north-narrow/annotation/*.txt countryside-natural/north-narrow/annotation
+scp -r erik@192.168.178.235:/home/erik/tno/datasets/MIDGARD/countryside-natural/north-fisheye/annotation/*.txt countryside-natural/north-fisheye/annotation
+scp -r erik@192.168.178.235:/home/erik/tno/datasets/MIDGARD/countryside-natural/south-narrow/annotation/*.txt countryside-natural/south-narrow/annotation
+scp -r erik@192.168.178.235:/home/erik/tno/datasets/MIDGARD/countryside-natural/south-fisheye/annotation/*.txt countryside-natural/south-fisheye/annotation
+scp -r erik@192.168.178.235:/home/erik/tno/datasets/MIDGARD/indoor-historical/church/annotation/*.txt indoor-historical/church/annotation
+scp -r erik@192.168.178.235:/home/erik/tno/datasets/MIDGARD/indoor-historical/stairwell/annotation/*.txt indoor-historical/stairwell/annotation
+scp -r erik@192.168.178.235:/home/erik/tno/datasets/MIDGARD/indoor-modern/glass-cube/annotation/*.txt indoor-modern/glass-cube/annotation
+scp -r erik@192.168.178.235:/home/erik/tno/datasets/MIDGARD/indoor-modern/sports-hall/annotation/*.txt indoor-modern/sports-hall/annotation
+scp -r erik@192.168.178.235:/home/erik/tno/datasets/MIDGARD/indoor-modern/warehouse-interior/annotation/*.txt indoor-modern/warehouse-interior/annotation
+scp -r erik@192.168.178.235:/home/erik/tno/datasets/MIDGARD/indoor-modern/warehouse-transition/annotation/*.txt indoor-modern/warehouse-transition/annotation
+scp -r erik@192.168.178.235:/home/erik/tno/datasets/MIDGARD/outdoor-historical/church/annotation/*.txt outdoor-historical/church/annotation
+scp -r erik@192.168.178.235:/home/erik/tno/datasets/MIDGARD/semi-urban/island-north/annotation/*.txt semi-urban/island-north/annotation
+scp -r erik@192.168.178.235:/home/erik/tno/datasets/MIDGARD/semi-urban/island-south/annotation/*.txt semi-urban/island-south/annotation
+scp -r erik@192.168.178.235:/home/erik/tno/datasets/MIDGARD/urban/appartment-buildings/annotation/*.txt urban/appartment-buildings/annotation
