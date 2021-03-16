Fix calculation of history
Former-commit-id: f6e5c7a9cbf6b5a781eacb4d963ae8a962819c30 [formerly 21799b1f74513815de9ee8a4e151294600b980ea]
Former-commit-id: 3af728b10a31b418549a97afbe3f0a9f9a4f831c

src/detector.py
@@ -336,6 +336,14 @@ class Detector:
 
         return cv2.cvtColor(np.around(img * 255 / max_intensity).astype(np.uint8), cv2.COLOR_GRAY2RGB)
 
+    def to_int(self, img: np.ndarray, type: type=np.uint8) -> np.ndarray:
+        return np.around(img).astype(type)
+
+    def clip(self, img: np.ndarray) -> np.ndarray:
+        img[..., 0] = np.clip(img[..., 0], 0, img.shape[1] - 1)
+        img[..., 1] = np.clip(img[..., 1], 0, img.shape[0] - 1)
+        return img
+
     def get_history(self, flow_uv: np.ndarray) -> np.ndarray:
         """Calculates the averaged tracked history of flow vectors.
 
@@ -346,23 +354,20 @@ class Detector:
             np.ndarray: history of flow field
         """
         self.flow_uv_history[self.history_index, ...] = flow_uv
-        self.flow_map_history[self.history_index, ...] = self.flow_uv_warped_mag
+
         k = (self.history_index + 2) % (self.history_length - 1)
-        summed_mag = self.flow_map_history[(self.history_index + 1) % (self.history_length - 1), ...]
+        orig_map = np.zeros_like(flow_uv, dtype=np.float64)
+        orig_map[..., 0] = self.y_coords
+        orig_map[..., 1] = self.x_coords
+        lookup_map = np.copy(orig_map)
 
-        while k != (self.history_index  + 1) % (self.history_length - 1):
-            lookup_x = self.x_coords + self.flow_uv_history[k, ..., 0]
-            lookup_y = self.y_coords + self.flow_uv_history[k, ..., 1]
-            lookup_x = np.around(np.clip(lookup_x, 0.0, summed_mag.shape[1] - 1)).astype(np.uint16)
-            lookup_y = np.around(np.clip(lookup_y, 0.0, summed_mag.shape[0] - 1)).astype(np.uint16)
-
-            summed_mag = summed_mag[lookup_y, lookup_x]
-            summed_mag += self.flow_map_history[(k - 1) % (self.history_length - 1), ...]
-
+        while k != (self.history_index + 1) % (self.history_length - 1):
+            warped = lookup_map.astype(np.float32)
+            lookup_map += cv2.remap(self.flow_uv_history[k, ...], warped[..., 1], warped[..., 0], cv2.INTER_LINEAR)
             k = (k + 1) % self.history_length
 
         self.history_index = (self.history_index + 1) % self.history_length
-        return self.to_rgb(summed_mag)
+        return lookup_map - orig_map
 
     def predict(self, segment: np.ndarray, flow_uv: np.ndarray, orig_frame: np.ndarray) -> None:
         avg = np.average(flow_uv[segment], 0)

src/focus_of_expansion.py
@@ -94,6 +94,15 @@ class FocusOfExpansion:
         return (FoE[0], FoE[1])
 
     def check_flow(self, flow_uv: np.ndarray, FoE: Tuple[float, float]) -> np.ndarray:
+        """Checks which parts of a flow field are parallel with the FoE.
+
+        Args:
+            flow_uv (np.ndarray): the flow field
+            FoE (Tuple[float, float]): the Focus of Expansion
+
+        Returns:
+            np.ndarray: BGR image with higher intensities for higher local motion.
+        """
         if FoE[0] is np.nan:
             return
 
@@ -102,7 +111,7 @@ class FocusOfExpansion:
         y_coords_foe = self.y_coords - FoE[1]
 
         diff1 = flow_uv
-        diff2 = np.zeros((x_coords_foe.shape[0], x_coords_foe.shape[1], 2))
+        diff2 = np.zeros_like(flow_uv)
         diff2[..., 0] = x_coords_foe
         diff2[..., 1] = y_coords_foe
 
@@ -110,7 +119,7 @@ class FocusOfExpansion:
         img_distance = np.linalg.norm(diff2, axis=2)
 
         angle_diff = (diff1[..., 0] * diff2[..., 0] + diff1[..., 1] * diff2[..., 1]) / (flow_magnitude * img_distance)
-        angle_diff = angle_diff * 255 / self.threshold * (flow_magnitude > 1.0)
+        angle_diff = angle_diff * 255 / self.threshold# * (flow_magnitude > 10.0)
         return angle_diff.astype(np.uint8)
 
     def draw(self, frame: np.ndarray, FoE: Tuple[float, float]) -> np.ndarray:

src/processor.py
@@ -284,14 +284,18 @@ class Processor:
             else:
                 self.detection_results[self.frame_index] = FrameResult()
                 self.flow_uv = self.dataset.get_flow_uv(self.frame_index)
+                self.flow_uv[..., 0] /= self.detector.get_magnitude(self.flow_uv)
+                self.flow_uv[..., 1] /= self.detector.get_magnitude(self.flow_uv)
+                self.flow_averaged = self.detector.get_history(self.flow_uv)
                 self.flow_vis = get_flow_vis(self.flow_uv)
 
                 FoE = self.focus_of_expansion.get_FOE(self.old_frame, orig_frame)
-                img = self.focus_of_expansion.check_flow(self.flow_uv, FoE)
+                img = self.focus_of_expansion.check_flow(self.flow_averaged, FoE)
                 self.old_frame = orig_frame
+                out_img = get_flow_vis(self.flow_averaged)
 
-                if img is not None and np.sum(img) > 0:
-                    self.write(img)
+                if out_img is not None and np.sum(out_img) > 0:
+                    self.write(out_img)
 
         return self.detection_results
 