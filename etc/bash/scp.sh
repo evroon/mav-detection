cdb7505e296598489df645b38caac86569080b5a
evroon <erik.vroon22@gmail.com>
2021-03-18 22:04:50 +0100

    Fix small bugs
    Former-commit-id: 375b03b7c7b4e4b1936fef616f5dcb49f2ff4072 [formerly ae40ca0b9fbeeb3265dc4e278483e769573a6d5f]
    Former-commit-id: f31d2bd2f9a750bf687243a45c2834d33667a3d5

--- etc/bash/scp.sh
+++ etc/bash/scp.sh
@@ -3,4 +3,4 @@ FRAME=127
 
 scp -r erik@192.168.178.235:/home/erik/tno/datasets/MIDGARD/indoor-modern/sports-hall .
 
-scp -r /mnt/c/Users/Daniel/Documents/uni/thesis/optical-flow-mav-detection/data erik@192.168.178.235:~/tno/datasets/sim-data
+scp -r /mnt/c/Users/Daniel/Documents/uni/thesis/optical-flow-mav-detection/data/* erik@192.168.178.235:~/tno/datasets/sim-data
