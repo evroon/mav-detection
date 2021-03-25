11106a9c095892e7d1efd415bb38c86f2618b070
Erik Vroon <erik.vroon22@gmail.com>
2021-03-25 14:20:05 +0100

    Calculate optical flow image
    Former-commit-id: f1eedf611f1cb250f5f157f3171630b5e9b7c058 [formerly bdac1e36832026323284701024e3aff27bb8c48c]
    Former-commit-id: 0a15f275b632fc1a276991e4e1050ffa81310224

--- src/airsim-optical-flow.py
+++ src/airsim-optical-flow.py
@@ -6,8 +6,8 @@ from scipy.spatial.transform import Rotation
 import matplotlib.pyplot as plt
 import airsim
 import cv2
-from sklearn.preprocessing import normalize
-from typing import List, Tuple, IO
+from im_helpers import get_flow_vis
+from typing import List, Tuple, Dict, Any, cast
 
 def mat4x4_vec4_mult(mat4x4: np.ndarray, vec4: np.ndarray) -> np.ndarray:
     return np.array(
@@ -36,7 +36,6 @@ def world_to_screen(view_proj: np.ndarray, screen_res: Tuple[int, int], world_po
     result = np.zeros((world_pos.shape[0], world_pos.shape[1], 2))
     result[..., 0] = normalized_x * screen_res[0]
     result[..., 1] = normalized_y * screen_res[1]
-
     return result
 
 def screen_to_world(view_proj_inv: np.ndarray, screen_res: Tuple[int, int], screen_pos: np.ndarray, depth: np.ndarray) -> np.ndarray:
@@ -75,47 +74,71 @@ def screen_to_world(view_proj_inv: np.ndarray, screen_res: Tuple[int, int], scre
 
     return result
 
-def get_view_proj_mat(f: IO) -> np.ndarray:
-    state_dict = json.load(f)
-    view_proj_str = state_dict['Drone1']['viewProjectionMatrix']
+def get_state(filepath: str) -> Dict[str, Any]:
+    with open(filepath, 'r') as f:
+        return cast(Dict[str, Any], json.load(f))
+
+def get_view_proj_mat(state: dict) -> np.ndarray:
+    view_proj_str = state['ue4']['Drone1']['viewProjectionMatrix']
     view_proj = view_proj_str.replace('[', '').replace(']', '').strip().split(' ')
     view_proj = [float(x) for x in view_proj]
     return np.array(view_proj).reshape((4, 4)).T
 
+def calculate_flow(
+        view_proj1: np.ndarray,
+        view_proj2: np.ndarray,
+        screen_res: np.ndarray,
+        screen_pos2: np.ndarray,
+        depth_img: np.ndarray,
+        drone_velocity: Dict[str, float],
+        segmentation: np.ndarray) -> np.ndarray:
+
+    view_proj2_inv = np.linalg.inv(view_proj2)
+
+    world_pos = screen_to_world(view_proj2_inv, screen_res, screen_pos2, depth_img)
+
+    # Subtract the drone velocity for all pixels in the mask in 3D world (unit: meters) frame.
+    drone_mask = segmentation > 0
+    world_pos[drone_mask, 0] -= drone_velocity['X']
+    world_pos[drone_mask, 1] -= drone_velocity['Y']
+    world_pos[drone_mask, 2] -= drone_velocity['Z']
+
+    screen_pos1 = world_to_screen(view_proj1, screen_res, world_pos)
+    return screen_pos1 - screen_pos2
+
 def get_flow() -> np.ndarray:
-    dir = 'data/states'
-    states = glob.glob(f'{dir}/*.json')
+    base_path = 'data/mountains-moving/lake-north-medium-5.0-10-default'
+    # base_path = 'data/mountains-stationary/lake-north-low-2.5-10-default'
+    states_dir = f'{base_path}/states'
+    depths_dir = f'{base_path}/depths'
+    segmentations_dir = f'{base_path}/segmentations'
+    screen_res = (1920, 1080)
+
+    states = glob.glob(f'{states_dir}/*.json')
     states = [x for x in states if 'timestamp' not in x]
     states.sort()
-    screen_res = (1920, 1080)
 
     x_coords = np.tile(np.arange(screen_res[0]), (screen_res[1], 1))
     y_coords = np.tile(np.arange(screen_res[1]), (screen_res[0], 1)).T
     coords = np.stack((x_coords, y_coords)).swapaxes(0, 2)
 
-    for i, state in enumerate(states[::-1]):
-        with open(state, 'r') as f:
-            view_proj = get_view_proj_mat(f)
+    for i, _ in enumerate(states[1:]):
+        state1 = get_state(states[i-1])
+        state2 = get_state(states[i])
 
-            depth_img = np.array(
-                airsim.read_pfm('data/mountains-stationary/lake-north-low-2.5-10-default/depths/image_00002.png')[0]
-            ).T
+        view_proj1 = get_view_proj_mat(state1)
+        view_proj2 = get_view_proj_mat(state2)
 
-            result = np.zeros_like(depth_img)
-            view_proj_inv = np.linalg.inv(view_proj)
+        drone_velocity = state1['ue4']['Drone2']['linearVelocity']
 
-            world_pos = screen_to_world(view_proj_inv, screen_res, coords, depth_img)
-            screen_pos = world_to_screen(view_proj, screen_res, world_pos)
+        depth_img = np.array(
+            airsim.read_pfm(f'{depths_dir}/image_{i+25:05d}.pfm')[0]
+        ).T
 
-            screen_pos = np.maximum(screen_pos, 0)
-            screen_pos[..., 0] = np.minimum(screen_pos[..., 0], screen_res[0] - 1)
-            screen_pos[..., 1] = np.minimum(screen_pos[..., 1], screen_res[1] - 1)
+        segmentation_img = cv2.imread(f'{segmentations_dir}/image_{i+25:05d}.png', 0).T
 
-            screen_pos = np.round(screen_pos).astype(np.uint16)
-
-            result[screen_pos[..., 0], screen_pos[..., 1]] = 255
-            cv2.imwrite('result.png', result.T)
-            return
+        result = calculate_flow(view_proj1, view_proj2, screen_res, coords, depth_img, drone_velocity, segmentation_img)
+        cv2.imwrite(f'media/result_{i:05d}.png', get_flow_vis(result).swapaxes(0, 1))
 
 
 get_flow()
