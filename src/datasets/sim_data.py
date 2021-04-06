Fix gt flow scaling bug
Former-commit-id: 99babad67a07643a19861490b8a3fa7d8a32c727 [formerly a9a8a2669f15ebe80ed0de3afb7937762992cd0c]
Former-commit-id: b3831085fc8e07efd848c458939f6073aba1fdd5

src/datasets/sim_data.py
@@ -70,7 +70,7 @@ class SimData(Dataset):
 
     def get_gt_foe(self, i:int) -> Optional[Tuple[float, float]]:
         FoE = self.get_state(i)['ue4']['Drone1']['FoE']
-        return (FoE['X'] * 800, FoE['Y'] * 600)
+        return (FoE['X'] * self.capture_size[0], FoE['Y'] * self.capture_size[1])
 
     def get_gt_of(self, i:int) -> Optional[np.ndarray]:
         return utils.read_flow(f'{self.gt_of_path}/image_{i:05d}.flo')